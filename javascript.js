fe4e52d6cef6451433f9bad439ad39a75d71b435
Dan <dpedersen84@gmail.com>
2018-03-29 21:43:24 -0500

    click event

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    console.log("Hello")
+    console.log("Hello!")
 
     // Initialize Firebase
     var config = {
@@ -16,6 +16,42 @@ $(document).ready(function() {
 
     //VARIABLES
     //---------------------------------------------------------------
+
+    var database = firebase.database();
+    
+    //Submit Event
+    $("#submitTrain").on("click", function (event) {
+        event.preventDefault();
+
+        var trainName = $("#train_name").val().trim();
+        var trainDestination = $("#destination").val().trim();
+        var trainTime = $("#train_time").val().trim();
+        var trainFrequency = $("#frequency").val().trim();
+
+        var newTrain = {
+            name: trainName,
+            destination:trainDestination,
+            time: trainTime,
+            frequency: trainFrequency 
+        };
+
+        database.ref().push(newTrain);
+
+        console.log(newTrain.name);
+        console.log(newTrain.destination);
+        console.log(newTrain.time);
+        console.log(newTrain.frequency);
+
+        $("#train_name").val("");
+        $("#destination").val("");
+        $("#train_time").val("");
+        $("#frequency").val("");
+    })
+
+
+
+
+
     //FUNCTIONS + EVENTS
     //---------------------------------------------------------------
     //MAIN PROCESS
