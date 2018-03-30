617fcfdb5d8d78d89aa801b236f2177278c3d9f5
Dan <dpedersen84@gmail.com>
2018-03-29 19:50:15 -0500

    firebase config

--- javascript.js
+++ javascript.js
@@ -1,1 +1,42 @@
-console.log("Hello")
+$(document).ready(function() {
+
+    console.log("Hello")
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAdKvCDt7waNKUjvnxsDPOw0kaF_rjbTqo",
+        authDomain: "fir-train-fc877.firebaseapp.com",
+        databaseURL: "https://fir-train-fc877.firebaseio.com",
+        projectId: "fir-train-fc877",
+        storageBucket: "fir-train-fc877.appspot.com",
+        messagingSenderId: "754658335762"
+    };
+  
+    firebase.initializeApp(config);
+
+    //VARIABLES
+    //---------------------------------------------------------------
+    //FUNCTIONS + EVENTS
+    //---------------------------------------------------------------
+    //MAIN PROCESS
+    //---------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
